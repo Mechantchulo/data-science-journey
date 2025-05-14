92bf4012b5dd576df5a2e9672d09eadfa7f7266b
Mechantchulo <chulomechant@gmail.com>
2025-05-14 23:23:21 +0300

    Variables exercises added

---
+++ 01-python-fundamentals/variables.py
@@ -0,0 +1,16 @@
+
+# Define variables with different data types
+integer_var = 42
+float_var = 3.14
+string_var = "Hello, World!"
+boolean_var = True
+list_var = [1, 2, 3, 4, 5]
+dict_var = {"key1": "value1", "key2": "value2"}
+
+# Print the variables to show their values
+print("Integer:", integer_var)
+print("Float:", float_var)
+print("String:", string_var)
+print("Boolean:", boolean_var)
+print("List:", list_var)
+print("Dictionary:", dict_var)
