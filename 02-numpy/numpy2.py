910de715d308118838770ade860fb3fa09497b61
Mechantchulo <chulomechant@gmail.com>
2025-05-17 23:01:36 +0300

    numpy array creation

---
+++ 02-numpy/numpy2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+#creating an array
+
+'''NumPy is used to work with arrays. The array object in NumPy is called ndarray.
+
+We can create a NumPy ndarray object by using the array() function.'''
+
+#creating a 1D array
+
+arr = np.array([1,2,3,4,5])
+print(arr)
